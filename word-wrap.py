4b45dcf032fc854376efdf21bd4bb7046f13d588
stephanie nguyen <47327419+stephanie-nguyen@users.noreply.github.com>
2021-09-01 13:39:41 -0500

    Update word-wrap.py

--- word-wrap.py
+++ word-wrap.py
@@ -1,12 +1,22 @@
 '''
 Given a string sentence, loop through string and add line break after every 10 characters.
 '''
-
+#method 1:
 def word_wrap(sentence,line_length):
   lines=[]
   for i in range(0, len(sentence), line_length):
     lines.append(sentence[i:i+line_length]+ "\n")
   return "".join(lines)
+
+
+'''
+#method 2:
+def word_wrap(sentence,line_length):
+  lines=[]
+  lines = [sentence[i:i+line_length] + '\n' for i in range(0, len(sentence), line_length)]
+  return "".join(lines)
+'''
+
 
 sentence="The quick brown fox jumps over the lazy dog"
 line_length=10
